d453c161566f1e0822daf249242372d5b537a17b
Joao Soutto <joaosoutto@hotmail.com>
2020-08-01 18:51:32 -0300

    tela de explorar ok, passando nos testes

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../Components/Footer';
 
 const Explore = () => (
   <div>
+    <Link to="/explorar/comidas">
+      <button data-testid="explore-food">Explorar Comidas</button>
+    </Link>
+    <Link to="/explorar/bebidas">
+      <button data-testid="explore-drinks">Explorar Bebidas</button>
+    </Link>
     <Footer />
   </div>
 );
