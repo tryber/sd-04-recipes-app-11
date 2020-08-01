a3505ac5ac75d149ba5585d642b7d8e2eea5d1fd
Joao Soutto <joaosoutto@hotmail.com>
2020-08-01 20:06:23 -0300

    tela explorar

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import Footer from '../Components/Footer';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Footer from '../Components/Footer';
 
-const ExploreDrinks = () => (
-  <div>
-    <Footer />
-  </div>
-);
+// const ExploreDrinks = () => (
+//   <div>
+//     <Link to="/explorar/bebidas/ingredientes">
+//       <button data-testid="explore-by-ingredient">Por Ingredientes</button>
+//     </Link>
+//     <Link to="">
+//       <button data-testid="explore-surprise">Me Surpreenda!</button>
+//     </Link>
+//     <Footer />
+//   </div>
+// );
 
-export default ExploreDrinks;
+// export default ExploreDrinks;
+
+// precisa das telas de receita.
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import Footer from '../Components/Footer';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Footer from '../Components/Footer';
 
-const ExploreFoods = () => (
-  <div>
-    <Footer />
-  </div>
-);
+// const ExploreFoods = () => (
+//   <div>
+//     <Link to="/explorar/comidas/ingredientes">
+//       <button data-testid="explore-by-ingredient">Por Ingredientes</button>
+//     </Link>
+//     <Link to="/explorar/comidas/area">
+//       <button data-testid="explore-by-area">Por Local de Origem</button>
+//     </Link>
+//     <Link to="">
+//       <button data-testid="explore-surprise">Me Surpreenda!</button>
+//     </Link>
+//     <Footer />
+//   </div>
+// );
 
-export default ExploreFoods;
+// export default ExploreFoods;
+
+//  precisa das telas de receita.
